4147d52012477127dcbdb5574dcdb0938a31e261
ShvaykaD <mitiabmx@gmail.com>
2019-02-21 16:47:20 +0200

    modified rule-nodes with md-chips

--- src/app/rulenode/config/locale/rulenode-core-locale.constant.js
+++ src/app/rulenode/config/locale/rulenode-core-locale.constant.js
@@ -277,7 +277,8 @@ export default function addRuleNodeCoreLocaleEnglish($translateProvider) {
                 "interval-seconds-required": "Interval is required.",
                 "min-interval-seconds-message": "Only 1 second minimum interval is allowed.",
                 "output-timeseries-key-prefix": "Output timeseries key prefix",
-                "output-timeseries-key-prefix-required": "Output timeseries key prefix required."
+                "output-timeseries-key-prefix-required": "Output timeseries key prefix required.",
+                "separator-hint": "You should press \"enter\" to complete field input."
             },
             "key-val": {
                 "key": "Key",
